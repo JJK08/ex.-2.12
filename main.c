9046b33555b505f84e57f219f280fe9e67565f1c
JJK08 <jinjiakai2008@gmail.com>
2024-09-21 16:38:39 +0200

    esercizio 2.1-2.3

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int main() {
+    float num1;
+    float num2;
+    float media;
+    printf("inserisci il primo numero: ");
+    scanf("%f",&num1);
+    printf("inserisci il secondo numero: ");
+    scanf("%f",&num2);
+    media=(num1+num2)/2;
+    printf("la media dei due numeri è:%f\n",media);
     return 0;
 }

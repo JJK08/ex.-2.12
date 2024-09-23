f2c9e4516ab3332b6c361583a1014477c39649ac
JJK08 <jinjiakai2008@gmail.com>
2024-09-23 15:17:11 +0200

    es 2.12

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 int main() {
-    float b;
-    float h;
-    float a;
-    printf("base del triangolo: ");
-    scanf("%f",&b);
-    printf("altezza del triangolo: ");
-    scanf("%f",&h);
-    a= (b*h)/2;
-    printf("l'area del triangolo e':%f\n",a);
+    int gg1,gg2,me1,me2,an1,an2,diff;
+    printf("inserire la prima dat del formato dd mm yyyy: ");
+    scanf("%d %d %d",&gg1,&me1,&an1);
+    printf("inserire la seconda data del formato dd mm yyyy: ");
+    scanf("%d %d %d",&gg2,&me2,&an2);
+    diff=gg2-gg1+(me2-me1)*30+(an2-an1)*3600;
+    gg1=diff%30;
+    me1=(diff/30)%12;
+    an1=diff/360;
+    printf("la differenza tra le due anni e': %d gg,%d me;%d an\n",gg1,me1,an1);
     return 0;
 }

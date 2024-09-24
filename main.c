fa5acb68b05c16872c4f16e7fbb0cde7f42c9ff8
JJK08 <jinjiakai2008@gmail.com>
2024-09-24 14:59:49 +0200

    es2.12

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 int main() {
-    float prezzo_biglietto;
-    float provvigione;
-    float prezzo_finale;
-    printf("inserisci il prezzo del biglietto: ");
-    scanf("%f",&prezzo_biglietto);
-    provvigione=prezzo_biglietto*0.15;
-    prezzo_finale=prezzo_biglietto-provvigione;
-    printf("valore del provvigione:%.2feuro\n",provvigione);
-    printf("prezzo finale del biglietto:%.2feuro\n",prezzo_finale);
+    int giorno1,giorno2,mesi1,mesi2,anno1,anno2,diff;
+    printf("inserisci la prima data in forma dd mm yyyy: ");
+    scanf("%d %d %d",&giorno1,&mesi1,&anno1);
+    printf("inserisci la seconda data in forma dd mm yyyy: ");
+    scanf("%d %d %d",&giorno2,&mesi2,&anno2);
+    diff=giorno2-giorno1+(mesi2-mesi1)*30+(anno2-anno1)*360;
+    giorno1=diff%30;
+    giorno2=(diff/30)%12;
+    anno1=diff/360;
+    printf("la differenza tra due date e':%d giorni,%d mesi,%d anno",giorno1,mesi1,anno1);
     return 0;
 }
